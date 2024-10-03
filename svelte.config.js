8b18448e16d052d06c6cec1da45943f957ae1f6e
Juan Ruiz <jruizt96@gmail.com>
2024-10-03 14:08:30 -0500

    adapter node

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { resolve } from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +12,19 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter({
+			out: 'build',
+			precompress: true
+		}),
+		alias: {
+			'@controllers': resolve('./src/controllers'),
+			'@models': resolve('./src/models'),
+			'@views': resolve('./src/views'),
+			'@routes': resolve('./src/routes'),
+			'@server': resolve('./src/server'),
+			'@assets': resolve('./src/assets'),
+			'@/*': './path/to/lib/*'
+		},
 	}
 };
 
